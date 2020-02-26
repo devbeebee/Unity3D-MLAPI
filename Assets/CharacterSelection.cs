7c3987b2de4d4c2aeb459ca2699d02964e201bec
devbeebee <devbeebee@outlook.com>
2020-02-26 11:18:36 +0000

    Added Basic Selection

--- Assets/CharacterSelection.cs
+++ Assets/CharacterSelection.cs
@@ -7,4 +7,22 @@ public class CharacterSelection : MonoBehaviour
     [Header("Place Characters as a child of this transform")]
     List<GameObject> playerCharacters;
     public List<GameObject> PlayerCharacters { get => playerCharacters; set => playerCharacters = value; }
+    int selection = 0;
+
+    void ChangeLeft()
+    {
+        selection--;
+        if (selection < 0)
+        {
+            selection = PlayerCharacters.Count - 1;
+        }
+    }
+    void ChangeRight()
+    {
+        selection++;
+        if (selection > PlayerCharacters.Count - 1)
+        {
+            selection = 0;
+        }
+    }
 }
