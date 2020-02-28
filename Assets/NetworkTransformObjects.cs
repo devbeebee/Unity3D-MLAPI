c62fc861fc05266c78fffe5b3df00a612da4041a
devbeebee <devbeebee@outlook.com>
2020-02-28 09:56:21 +0000

    Added Parenting of networked objects

---
+++ Assets/NetworkTransformObjects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkTransformObjects : MonoBehaviour
+{
+    public static NetworkTransformObjects Instance { get { return _instance; } }
+    static NetworkTransformObjects _instance;
+
+    public Transform PlayerParent;
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+}
--- Assets/PlayerNetworkStart.cs
+++ Assets/PlayerNetworkStart.cs
@@ -1,29 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using MLAPI;
-using MLAPI.Transports.UNET;
+﻿using UnityEngine;
 
-public class PlayerNetworkStart : NetworkedBehaviour
+public class PlayerNetworkStart : MonoBehaviour
 {
     public static PlayerNetworkStart Instance { get { return _instance; } }
     static PlayerNetworkStart _instance;
-    [SerializeField]
-    bool drawOnGUI = false;
-    [SerializeField]
-    bool network = false;
-    public UnetTransport unetTransport;
-    private void OnGUI()
-    {
-        if (Application.isEditor && drawOnGUI && network)
-        {
-            GUILayout.Label($"{unetTransport.ConnectAddress},{unetTransport.ConnectPort}");
-        }
-    }
+   
 
-    public override void NetworkStart()
+    public void Start()
     {
-        unetTransport = NetworkingManager.Singleton.GetComponent<UnetTransport>();
-        network = true;
+        transform.SetParent(NetworkTransformObjects.Instance.PlayerParent);
     }
 }
