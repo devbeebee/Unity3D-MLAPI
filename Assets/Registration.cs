a9c1de50727d940227609999e0f6ce19438cde5c
devbeebee <devbeebee@outlook.com>
2020-02-28 13:11:09 +0000

    Added Mysql dll

---
+++ Assets/Registration.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System;
+using System.Data;
+using System.Text;
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+public class Registration : MonoBehaviour
+{
+    public string host, database, user, password;
+    public bool pooling = true;
+
+    private string connectionString;
+    private MySqlConnection con = null;
+    private MySqlCommand cmd = null;
+    private MySqlDataReader rdr = null;
+
+    private MD5 _md5Hash;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        connectionString = "Server=" + host + ";Database=" + database + ";User=" + user + ";Password=" + password + ";Pooling=";
+        if (pooling)
+        {
+            connectionString += "True";
+        }
+        else
+        {
+            connectionString += "False";
+        }
+        try
+        {
+            con = new MySqlConnection(connectionString);
+            con.Open();
+            Debug.Log("Mysql state: " + con.State);
+
+            string sql = "SELECT * FROM clothes";
+            cmd = new MySqlCommand(sql, con);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+    void onApplicationQuit()
+    {
+        if (con != null)
+        {
+            if (con.State.ToString() != "Closed")
+            {
+                con.Close();
+                Debug.Log("Mysql connection closed");
+            }
+            con.Dispose();
+        }
+    }
+
+    public string getFirstShops()
+    {
+        using (rdr = cmd.ExecuteReader())
+        {
+            while (rdr.Read())
+            {
+                return rdr[0] + " -- " + rdr[1];
+            }
+        }
+        return "empty";
+    }
+    public string GetConnectionState()
+    {
+        return con.State.ToString();
+    }
+}
--- Mamp PHP/README.md
+++ Mamp PHP/README.md
@@ -1,4 +1,4 @@
-# Unity3D-MLAPI
+# PHP SERVER
 Mamp can be found here : https://www.mamp.info/en/downloads/
 
 Version i used/ am using : Windows 4.1.1
